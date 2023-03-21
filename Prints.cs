73ea0a341683f816772aa5ab9836e3c2057d2512
fonseca9001 <fonsecajoao@live.com>
2023-03-21 18:05:31 +0000

    Added logo v1

--- Prints.cs
+++ Prints.cs
@@ -9,7 +9,15 @@ namespace euromilhoes
     {
         void printLogo()
         {
-            Console.WriteLine("");
+            string asciiArt = @"
+                ______                           _ ____
+               / ____/_  ___________  ____ ___  (_) / /_  ____  ___  _____
+              / __/ / / / / ___/ __ \/ __ `__ \/ / / __ \/ __ \/ _ \/ ___/
+             / /___/ /_/ / /  / /_/ / / / / / / / / / / / /_/ /  __(__  )
+            /_____/\__,_/_/   \____/_/ /_/ /_/_/_/_/ /_/\____/\___/____/
+                 A criar excentricos todas as DateTime.Now.AddDays(8)";
+            
+            Console.WriteLine(asciiArt);
         }
         void printMenu()
         {
