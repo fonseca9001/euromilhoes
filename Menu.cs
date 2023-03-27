daffb15321aa0e6fba5f0b1271fdaf479f2afe73
Mowta <124155538+Mowta@users.noreply.github.com>
2023-03-27 21:15:29 +0100

    Add files via upload

--- Menu.cs
+++ Menu.cs
@@ -37,37 +37,53 @@ namespace Euromilhoes
             int opcao;
             if (!int.TryParse(Console.ReadLine(), out opcao))
             {
-                Console.WriteLine("Opção inválida.");
+                Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                Console.WriteLine("┃ ❌ Opção inválida. ❌ ┃");
+                Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━┛");
                 continue;
             }
 
             switch (opcao)
             {
                 case 0:
-                    Console.WriteLine("Obrigado por jogar!");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃🤡brigado por j🤡gar! ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
                     return;
                 case 1:
-                    Console.WriteLine("Quantas chaves deseja gerar?");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃Quantas 🔑 deseja gerar?┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━┛");
                     int quantidade;
                     if (!int.TryParse(Console.ReadLine(), out quantidade) || quantidade <= 0)
                     {
-                        Console.WriteLine("Quantidade inválida.");
+                        Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                        Console.WriteLine("┃ ❌ Quantidade inválida. ❌ ┃");
+                        Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
                         continue;
                     }
-                    Console.WriteLine("Digite o seu NIF:");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃   Digite o seu NIF:   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
                     string nif = Console.ReadLine();
                     euromilhoes.GerarChavesAutomaticas(quantidade, nif);
                     Console.WriteLine($"Foram geradas {quantidade} chaves automáticas para o NIF {nif}.");
                     break;
                 case 2:
-                    Console.WriteLine("Digite a chave manual no formato '1,2,3,4,5':");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃    Digite a chave manual no formato '1,2,3,4,5':   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
                     string chave = Console.ReadLine();
-                    Console.WriteLine("Digite o seu NIF:");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃   Digite o seu NIF:   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
                     nif = Console.ReadLine();
                     euromilhoes.GerarChaveManual(chave, nif);
                     break;
                 case 3:
-                    Console.WriteLine("Digite o seu NIF:");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃   Digite o seu NIF:   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
                     nif = Console.ReadLine();
                     euromilhoes.FazerAposta(nif);
                     break;
@@ -75,36 +91,50 @@ namespace Euromilhoes
                     euromilhoes.VerificarApostas();
                     break;
                 case 5:
-                    Console.WriteLine("Digite o seu NIF:");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃   Digite o seu NIF:   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
                     nif = Console.ReadLine();
                     double saldo = euromilhoes.ConsultarSaldo(nif);
                     Console.WriteLine($"Saldo do NIF {nif}: {saldo}€");
                     break;
                 case 6:
-                    Console.WriteLine("Digite o seu NIF:");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃   Digite o seu NIF:   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
                     nif = Console.ReadLine();
-                    Console.WriteLine("Digite o valor a adicionar:");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃    Digite o valor a adicionar:   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
                     double valor;
                     if (!double.TryParse(Console.ReadLine(), out valor) || valor <= 0)
                     {
-                        Console.WriteLine("Valor inválido.");
+                        Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                        Console.WriteLine("┃ ❌ Valor inválido. ❌ ┃");
+                        Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━┛");
                         continue;
                     }
                     euromilhoes.AtualizarSaldo(nif, valor);
                     Console.WriteLine($"Saldo do NIF {nif}: {euromilhoes.ConsultarSaldo(nif)}€");
                     break;
                 case 7:
-                    Console.WriteLine("Digite o seu NIF:");
+                    Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃   Digite o seu NIF:   ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━┛");
                     nif = Console.ReadLine();
                     Console.WriteLine("Digite o valor a retirar:");
                     if (!double.TryParse(Console.ReadLine(), out valor) || valor <= 0)
                     {
-                        Console.WriteLine("Valor inválido.");
+                        Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                        Console.WriteLine("┃ ❌ Valor inválido. ❌ ┃");
+                        Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━┛");
                         continue;
                     }
                     if (euromilhoes.ConsultarSaldo(nif) < valor)
 					{
-						Console.WriteLine("Saldo insuficiente.");
+                        Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+						Console.WriteLine("┃💸  Saldo insuficiente.  💸┃");
+                        Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 						continue;
 					}
 						euromilhoes.AtualizarSaldo(nif, -valor);
@@ -117,7 +147,9 @@ namespace Euromilhoes
 					euromilhoes.MostrarTodasChavesGeradas();
 					break;
 				default:
-					Console.WriteLine("Opção inválida.");
+					Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━┓");
+                    Console.WriteLine("┃ ❌ Opção inválida. ❌ ┃");
+                    Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━┛");
 					break;
 			}
 		}
